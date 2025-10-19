6f0353a8adae3dbe94a43fd1c82610fc3c656f6e
Ricardo Moreno <ricardomoreno@gmail.com>
2025-10-19 13:57:59 -0600

    second commit

--- pages/contact.js
+++ pages/contact.js
@@ -9,16 +9,16 @@ export default function Contact(){
         <h1>Contacto</h1>
         <div style={{display:'grid',gridTemplateColumns:'1fr 320px',gap:20}}>
           <div>
-            <p>Tel: +52 1 442 XXX XXXX</p>
-            <p>Email: contacto@heliosengineering.com</p>
-            <p>Dirección: Querétaro, México</p>
+            <p>Tel: +52 6621384689 </p>
+            <p>Email: HeliosEngineeringCenter@gmail.com</p>
+            <p>Dirección: Hermosillo, México</p>
             <h3>Escríbenos</h3>
             <p>También puedes solicitar cotización desde la página principal.</p>
           </div>
           <div className="card">
             <strong>Horario</strong>
             <p>Lunes a Viernes 9:00 — 18:00</p>
-            <a className="cta" href="https://wa.me/521442XXXXXXX">Abrir WhatsApp</a>
+            <a className="cta" href="https://wa.me/526621384689">Abrir WhatsApp</a>
           </div>
         </div>
       </main>
